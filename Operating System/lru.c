a0062f10c4d464213253ff5f4ead92167746e1f9
julian5383 <julian5383@kku.ac.kr>
2022-06-26 23:41:52 +0900

    15번째 커밋

---
+++ Operating System/lru.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TRUE 1
+#define FALSE 0
+
+typedef struct node
+{
+	int num;
+	struct node* next;
+	clock_t time;
+}NODE;
+
+NODE* head;
+NODE* tail;
+
+void init()
+{
+	head = (NODE*)malloc(sizeof(NODE));
+	tail = (NODE*)malloc(sizeof(NODE));
+
+	head->next = tail;
+	tail->next = tail;
+}
+
+NODE* c_time() {
+	NODE* p = head->next;
+	NODE* s = p->next;
+	NODE* tmp = s;
+	clock_t start = clock();
+
+	double time1, time2;
+	while (s != tail)
+	{
+		time1 = (double)(start - tmp->time);
+		time2 = (double)(start - s->time);
+
+		if (time1 < time2)
+		{
+			tmp = s;
+		}
+		p = p->next;
+		s = p->next;
+	}
+	return tmp;
+}
+
+void insert(int num)
+{
+	NODE* p = malloc(sizeof(NODE));
+	NODE* in_head = head;
+	NODE* in_node = in_head->next;
+	p->num = num;
+
+	if (head->next == tail)
+	{
+		p->next = tail;
+		head->next = p;
+	}
+	else
+	{
+		while (in_node != tail)
+		{
+			in_head = in_head->next;
+			in_node = in_head->next;
+		}
+		in_head->next = p;
+		p->next = in_node;
+	}
+}
+
+void delete(int num)
+{
+	NODE* in_head = head;
+	NODE* in_node = in_head->next;
+	NODE* tmp;
+	NODE* new = (NODE*)malloc(sizeof(NODE));
+	new->num = num;
+
+	tmp = c_time(head, tail);
+
+	if (in_node != tmp)
+	{
+		in_head = in_head->next;
+		in_node = in_head->next;
+	}
+	in_head->next = new;
+	new->next = in_node->next;
+	new->time = clock();
+	free(in_node);
+}
+
+int search(int num)
+{
+	NODE* p = head->next;
+	while (p != tail)
+	{
+		if (p->num == num)
+		{
+			return 1;
+		}
+		p = p->next;
+	}
+	return 0;
+}
+
+int length_list()
+{
+	int length = 0;
+	NODE* p = head->next;
+	while (p != tail)
+	{
+		p = p->next;
+		length++;
+	}
+	return length;
+}
+
+int main()
+{
+	FILE* fp = NULL;
+	int i, num, nc = 0;
+	int arr[100000];
+	int hit = 0;
+	int node;
+
+	init();
+
+	printf("1: tr.2c39.txt\n");
+	printf("2: tr.db2.txt\n");
+	printf("3: tr.sort.txt\n");
+	printf("4: r.zipf_20_80.txt\n");
+	printf("Input file name's(1,2,3,4 only): ");
+	scanf("%d", &i);
+
+	printf("Input Node Number(100, 500, 1000 only) : ");
+	scanf("%d", &node);
+
+	switch (i)
+	{
+	case 1:
+		fp = fopen("tr.2c39.txt", "r");
+		break;
+	case 2:
+		fp = fopen("tr.db2.txt", "r");
+		break;
+	case 3:
+		fp = fopen("tr.sort.txt", "r");
+		break;
+	case 4:
+		fp = fopen("tr.zipf_20_80.txt", "r");
+		break;
+	default:
+		printf("Error\n");
+		break;
+	}
+
+	while (fscanf(fp, "%d", &num) > 0) {
+		nc++;
+
+		if (search(num)) {
+			hit++;
+		}
+
+		else
+		{
+			while (1)
+			{
+				if (length_list() < node)
+				{
+					insert(num);
+					break;
+				}
+				else
+				{
+					delete(num);
+					break;
+				}
+			}
+		}
+
+	}
+
+	fclose(fp);
+	printf("Hit rate: %.2f\n", ((double)hit / (double)nc) * (double)100);
+	return 0;
+}
